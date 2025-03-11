5fc33956149db248340111c0bf6dfea262e80c62
OukaMiyuki <asukaloid.blog@gmail.com>
2025-03-11 12:49:24 +0700

    minor change

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/api/payment/update', [PaymentConfirmController::class, 'updatePayment'])->name('api.payment.update');
+Route::post('/payment/update', [PaymentConfirmController::class, 'updatePayment'])->name('api.payment.update');
