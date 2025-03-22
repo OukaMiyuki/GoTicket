4687796ce77933da8811374061ad4aa94abee71f
OukaMiyuki <asukaloid.blog@gmail.com>
2025-03-22 15:43:31 +0700

    test pusher

--- resources/js/components/operator/PaymentUpdate.vue
+++ resources/js/components/operator/PaymentUpdate.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
     <div></div>
 </template>
 
@@ -26,6 +26,10 @@ export default {
                 key: 'a0f8e18348832696e61b',
                 cluster: 'ap1',
                 forceTLS: true,
+            });
+
+            echo.connector.pusher.connection.bind('connected', () => {
+                console.log('Connected to Pusher!');
             });
 
             echo.channel('payment-channel')
@@ -56,67 +60,4 @@ export default {
         }
     }
 };
-</script> -->
-
-<template>
-    <div></div>
-  </template>
-
-  <script>
-  import Echo from 'laravel-echo';
-  import Pusher from 'pusher-js';
-  import Swal from 'sweetalert2'; // Make sure SweetAlert2 is installed: npm install sweetalert2
-
-  window.Pusher = Pusher;
-
-  export default {
-    props: {
-      invoiceId: {
-        type: [Number, String],
-        required: true
-      }
-    },
-    mounted() {
-      console.log("Payment Update Vue component is mounted with invoiceId:", this.invoiceId);
-      this.setupPusher();
-    },
-    methods: {
-      setupPusher() {
-        const echo = new Echo({
-          broadcaster: 'pusher',
-          key: 'a0f8e18348832696e61b',
-          cluster: 'ap1',
-          forceTLS: true,
-        });
-
-        echo.channel('payment-channel')
-          .listen('App.Events.PaymentUpdated', (data) => {
-            console.log("Received event data:", data);
-            console.log("Component invoiceId:", this.invoiceId);
-            // Convert both values to strings for safe comparison
-            if (String(data.invoiceId) === String(this.invoiceId)) {
-              console.log("Invoice ID matched! Triggering success.");
-              this.handlePaymentSuccess();
-            } else {
-              console.log("Invoice ID did not match.");
-            }
-          });
-      },
-      handlePaymentSuccess() {
-        Swal.fire({
-          title: 'Payment Success',
-          text: 'Your payment has been processed successfully.',
-          icon: 'success',
-          confirmButtonText: 'OK'
-        }).then(() => {
-          this.redirectUser();
-        });
-      },
-      redirectUser() {
-        const url = `/operator/transaction/invoice/ticket/${this.invoiceId}`;
-        console.log("Redirecting to:", url);
-        window.location.href = url;
-      }
-    }
-  };
-  </script>
+</script>
